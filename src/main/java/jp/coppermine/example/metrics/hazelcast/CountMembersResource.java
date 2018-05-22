03bd7e3ba813e8daf8f540419193da420c7f73c1
HASUNUMA Kenji <k.hasunuma@coppermine.jp>
2018-05-22 18:34:40 +0900

    Add Gauge sample using Hazelcast

---
+++ src/main/java/jp/coppermine/example/metrics/hazelcast/CountMembersResource.java
@@ -0,0 +1,25 @@
+package jp.coppermine.example.metrics.hazelcast;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+
+import com.hazelcast.core.HazelcastInstance;
+
+@Path("hazelcast/members")
+@RequestScoped
+public class CountMembersResource {
+    
+    @Inject
+    private HazelcastInstance hazelcastInstance;
+    
+    @GET
+    @Gauge(unit = "unit", name = "hazelcast-cluster-members", absolute = true)
+    public int count() {
+        return hazelcastInstance.getCluster().getMembers().size();
+    }
+
+}
